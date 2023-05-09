61776501890791fd29b8331d1a0caf118c620a37
hades <93888938+hades1x@users.noreply.github.com>
2023-05-09 16:08:52 -0400

    Delete title.js

--- title.js
+++
@@ -1,8 +0,0 @@
-let titleInterval;
-let title = ["_", "c", "co", "conn", "conn#", "conn#6", "conn#63", "conn#634", "conn#6347",];
-
-let iter = 0;
-setInterval(() => {
-    document.title = title[iter++ % title.length]
-    if (iter >= title.length + 1) { iter = 0; }
-}, 350)
